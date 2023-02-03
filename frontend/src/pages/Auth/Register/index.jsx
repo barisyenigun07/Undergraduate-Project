7c2768e34bccf2165895d216c2807760db61d12c
gurcangul <ggurcangul@gmail.com>
2023-02-03 12:51:07 +0300

    footer and without login added

--- frontend/src/pages/Auth/Register/index.jsx
+++ frontend/src/pages/Auth/Register/index.jsx
@@ -11,38 +11,37 @@ import {
   Select,
   MenuItem,
   InputLabel,
-  FormControl
+  FormControl,
+  Link,
 } from "@mui/material";
 
-import {useFormik} from "formik";
-import validationSchema from './validations'
-
-
+import { useFormik } from "formik";
+import validationSchema from "./validations";
 
 const Register = () => {
-
   const formik = useFormik({
-    initialValues:{ 
+    initialValues: {
       name: "",
       username: "",
       phone: "",
       email: "",
       password: "",
-      passwordConfirm:""
+      passwordConfirm: "",
     },
-    onSubmit: async(values, bag) =>{
+    onSubmit: async (values, bag) => {
       console.log(values);
     },
-    validationSchema
-  })
-
+    validationSchema,
+  });
 
   return (
     <>
       <Grid container spacing={2}>
-        <Grid item xs={7}             
+        <Grid
+          item
+          xs={7}
           className="backgroundMain"
-          sx={{            
+          sx={{
             backgroundRepeat: "no-repeat",
             backgroundSize: "cover",
             position: "relative",
@@ -50,10 +49,9 @@ const Register = () => {
             justifyContent: "center",
             alignItems: "center",
             height: "100vh",
-        }}
-      >
-          <Box
-          >
+          }}
+        >
+          <Box>
             <Typography
               sx={{
                 fontSize: "44px",
@@ -78,97 +76,98 @@ const Register = () => {
                 </Typography>
                 <br /> <br />
               </Box>
-              <form 
-                  onSubmit={formik.handleSubmit}
-                >
-              <Box sx={{ px: 8,}}>
-                <TextField
-                  id="name"
-                  label="Name"
-                  variant="outlined"
-                  size="small"
-                  sx={{ width: 1, maxWidth: "540px" }}
-                  name="name"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.name}
-                  error={formik.touched.name && formik.errors.name}
-                />
-              </Box>
-              <Box sx={{ px: 8,  mt:2}}>
-                <TextField
-                  id="username"
-                  label="Username"
-                  variant="outlined"
-                  size="small"
-                  sx={{ width: 1, maxWidth: "540px" }}
-                  name="username"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.username}
-                  error={formik.touched.username && formik.errors.username}
-                />
-              </Box>
-              <Box sx={{ px: 8, mt:2 }}>
-                <TextField
-                  id="phone-number"
-                  label="Phone Number"
-                  variant="outlined"
-                  size="small"
-                  sx={{ width: 1, maxWidth: "540px" }}
-                  name="phone"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.phone}
-                  error={formik.touched.phone && formik.errors.phone}
-               />
-              </Box>
-              <Box sx={{ px: 8, mt:2 }}>
-                <TextField
-                  id="email"
-                  label="Email"
-                  variant="outlined"
-                  size="small"
-                  sx={{ width: 1, maxWidth: "540px" }}
-                  name="email"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.email}
-                  error={formik.touched.email && formik.errors.email}
-                 />
-              </Box>
-              <Box sx={{ px: 8,mt:2 }}>
-                <TextField
-                  id="password"
-                  label="Password"
-                  variant="outlined"
-                  size="small"
-                  sx={{ width: 1, maxWidth: "540px" }}
-                  name="password"
-                  type={"password"}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.password}
-                  error={formik.touched.password && formik.errors.password}
-                />
-              </Box>
-              <Box sx={{ px: 8, mt:2 }}>
-                <TextField
-                  id="password-again"
-                  label="Password Again"
-                  variant="outlined"
-                  size="small"
-                  sx={{ width: 1, maxWidth: "540px" }}
-                  name="passwordConfirm"
-                  type={"password"}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.passwordConfirm}
-                  error={formik.touched.passwordConfirm && formik.errors.passwordConfirm}
-                />
-              </Box>
+              <form onSubmit={formik.handleSubmit}>
+                <Box sx={{ px: 8 }}>
+                  <TextField
+                    id="name"
+                    label="Name"
+                    variant="outlined"
+                    size="small"
+                    sx={{ width: 1, maxWidth: "540px" }}
+                    name="name"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.name}
+                    error={formik.touched.name && formik.errors.name}
+                  />
+                </Box>
+                <Box sx={{ px: 8, mt: 2 }}>
+                  <TextField
+                    id="username"
+                    label="Username"
+                    variant="outlined"
+                    size="small"
+                    sx={{ width: 1, maxWidth: "540px" }}
+                    name="username"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.username}
+                    error={formik.touched.username && formik.errors.username}
+                  />
+                </Box>
+                <Box sx={{ px: 8, mt: 2 }}>
+                  <TextField
+                    id="phone-number"
+                    label="Phone Number"
+                    variant="outlined"
+                    size="small"
+                    sx={{ width: 1, maxWidth: "540px" }}
+                    name="phone"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.phone}
+                    error={formik.touched.phone && formik.errors.phone}
+                  />
+                </Box>
+                <Box sx={{ px: 8, mt: 2 }}>
+                  <TextField
+                    id="email"
+                    label="Email"
+                    variant="outlined"
+                    size="small"
+                    sx={{ width: 1, maxWidth: "540px" }}
+                    name="email"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.email}
+                    error={formik.touched.email && formik.errors.email}
+                  />
+                </Box>
+                <Box sx={{ px: 8, mt: 2 }}>
+                  <TextField
+                    id="password"
+                    label="Password"
+                    variant="outlined"
+                    size="small"
+                    sx={{ width: 1, maxWidth: "540px" }}
+                    name="password"
+                    type={"password"}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.password}
+                    error={formik.touched.password && formik.errors.password}
+                  />
+                </Box>
+                <Box sx={{ px: 8, mt: 2 }}>
+                  <TextField
+                    id="password-again"
+                    label="Password Again"
+                    variant="outlined"
+                    size="small"
+                    sx={{ width: 1, maxWidth: "540px" }}
+                    name="passwordConfirm"
+                    type={"password"}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.passwordConfirm}
+                    error={
+                      formik.touched.passwordConfirm &&
+                      formik.errors.passwordConfirm
+                    }
+                  />
+                </Box>
 
-              {/*
+                {/*
               <Box sx={{ px: 8, textAlign: 'left', mt:2 }}>
                 <FormControl fullWidth>
                   <InputLabel>Select the type of user</InputLabel>
@@ -184,26 +183,30 @@ const Register = () => {
               </Box>
 
               */}
-              <Box sx={{ px: 8
-                
-                , mt:2 }}>
-                <Button
-                  type="submit"
-                  variant="contained"
+                <Box
                   sx={{
-                    borderRadius: 5,
-                    bgcolor: "#8E1904",
-                    p: 1,
-                    width: 1,
-                    "&:hover": {
-                      bgcolor: "#571104",
-                    },
-                    maxWidth: "540px",
+                    px: 8,
+
+                    mt: 2,
                   }}
                 >
-                  Register
-                </Button>
-              </Box>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    sx={{
+                      borderRadius: 5,
+                      bgcolor: "#8E1904",
+                      p: 1,
+                      width: 1,
+                      "&:hover": {
+                        bgcolor: "#571104",
+                      },
+                      maxWidth: "540px",
+                    }}
+                  >
+                    Register
+                  </Button>
+                </Box>
               </form>
 
               <Divider>Or</Divider>
@@ -230,6 +233,7 @@ const Register = () => {
                   Login
                 </Button>
               </Box>
+              <Link href="/home">Üye Olmadan Devam Et</Link>
             </Stack>
           </Box>
         </Grid>
